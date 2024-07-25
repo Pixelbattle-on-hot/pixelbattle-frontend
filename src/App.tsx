cc9a6b8efde482dff473c0c6c0d97bfac434d570
Serhii <kaluhin.dev@gmail.com>
2024-07-26 01:35:34 +0300

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import {Palette} from "./components/Palette.tsx";
-import {Footer} from "./components/Footer.tsx";
+import "./App.css";
+import { Palette } from "./components/Palette.tsx";
+import { Footer } from "./components/Footer.tsx";
 
 function App() {
   return (
-    <>
-      <Palette />
-      <Footer />
-    </>
-  )
+    <main className="flex h-dvh flex-col items-center justify-between">
+      <header>₸ 250</header>
+      <canvas className="size-96 bg-white"></canvas>
+      <div className="flex flex-col">
+        <div>(11, 22) 0.48 USDT</div>
+        <button>Paint</button>
+      </div>
+      <div className="bg-lightBackground flex w-full flex-col">
+        <Palette />
+        <Footer />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      orbitron: ["Orbitron Variable", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        lightBackground: "#ffffe1",
+      },
+    },
   },
   plugins: [],
-}
-
+};
