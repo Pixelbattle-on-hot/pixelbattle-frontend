814a16c153b24a1438605860f8f6e5398953d312
HolidayMan <lesha.pitenko791@gmail.com>
2024-07-26 00:18:01 +0200

    added palette context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import {Palette} from "./components/Palette.tsx";
+import {Palette, ActiveColorContext} from "./components/Palette.tsx";
 import {Footer} from "./components/Footer.tsx";
 
 function App() {
+  const [currentActiveColor, setCurrentActiveColor] = useState<number>(-1);
   return (
-    <>
-      <Palette />
+    <ActiveColorContext.Provider value={currentActiveColor}>
+      <Palette setActiveColorContext={setCurrentActiveColor}/>
       <Footer />
-    </>
+    </ActiveColorContext.Provider>
   )
 }
 
